c5066f848d632ac94ac110887cd50003327a9d4a
Amit-Mishra <amitmishra31929@gmail.com>
2026-06-27 20:58:18 +0530

    Update submission-1.py

--- Data Structures & Algorithms/is-palindrome/submission-1.py
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -4,19 +4,19 @@ class Solution:
         right = len(s) - 1
 
         while left < right:
-            # Skip non-alphanumeric characters from the left
+            
             while left < right and not self.alphaNum(s[left]):
                 left += 1
 
-            # Skip non-alphanumeric characters from the right
+           
             while left < right and not self.alphaNum(s[right]):
                 right -= 1
 
-            # Compare characters (case-insensitive)
+          
             if s[left].lower() != s[right].lower():
                 return False
 
-            # Move both pointers inward
+           
             left += 1
             right -= 1
 
